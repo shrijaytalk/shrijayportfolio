37eba4b0b90ea7c19a5bf22ea60a329557accfa8
Nikita Balani <shrijaykesarwaniwork@gmail.com>
2021-03-11 13:41:20 +0530

    first commit

---
+++ build/precache-manifest.1be44ba89c38d9a7e8ea9e24a17b0164.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0dbec6c8da6439d80a2b425dc20a063",
+    "url": "/shrijayportfolio/index.html"
+  },
+  {
+    "revision": "ea336b5f6ace8a9ccbb1",
+    "url": "/shrijayportfolio/static/css/main.a793a9d3.chunk.css"
+  },
+  {
+    "revision": "83804634c7a6d1303285",
+    "url": "/shrijayportfolio/static/js/2.2f8b1523.chunk.js"
+  },
+  {
+    "revision": "176c1a1a28b19c611d9701905d8d60b7",
+    "url": "/shrijayportfolio/static/js/2.2f8b1523.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea336b5f6ace8a9ccbb1",
+    "url": "/shrijayportfolio/static/js/main.59ff52c0.chunk.js"
+  },
+  {
+    "revision": "e75724828005cd674356",
+    "url": "/shrijayportfolio/static/js/runtime-main.107736e3.js"
+  },
+  {
+    "revision": "8897aeb643b17c37ab222bf70378423c",
+    "url": "/shrijayportfolio/static/media/React_ToDo_List.8897aeb6.webp"
+  },
+  {
+    "revision": "9730fefc26bb187c4f4bb29b95d82e7a",
+    "url": "/shrijayportfolio/static/media/Robofriends.9730fefc.webp"
+  },
+  {
+    "revision": "bf989778565c49da91f9e0ef13dbdc5c",
+    "url": "/shrijayportfolio/static/media/background.bf989778.webp"
+  },
+  {
+    "revision": "e95d05630894e5403e569fdaa0a2b61a",
+    "url": "/shrijayportfolio/static/media/background.e95d0563.webp"
+  },
+  {
+    "revision": "ffded2381d25dd906406d6e07f91dea5",
+    "url": "/shrijayportfolio/static/media/bootstrap-4.ffded238.svg"
+  },
+  {
+    "revision": "3707da3c6a394c7ee2523273f8d98a25",
+    "url": "/shrijayportfolio/static/media/css3.3707da3c.svg"
+  },
+  {
+    "revision": "c6bab64b183f96aa47077fd3db37afd0",
+    "url": "/shrijayportfolio/static/media/express.c6bab64b.svg"
+  },
+  {
+    "revision": "929c9aca661dd6ec45a759fac63f2b76",
+    "url": "/shrijayportfolio/static/media/get_github_info.929c9aca.webp"
+  },
+  {
+    "revision": "1190a19ce3cc940741a60137a1e074cb",
+    "url": "/shrijayportfolio/static/media/git-icon.1190a19c.svg"
+  },
+  {
+    "revision": "0555bfb7e81f57a1efe6a53c7490abb7",
+    "url": "/shrijayportfolio/static/media/github-api.0555bfb7.svg"
+  },
+  {
+    "revision": "0ff1a6f08182be4aa63a0b54da1b9e57",
+    "url": "/shrijayportfolio/static/media/github.0ff1a6f0.svg"
+  },
+  {
+    "revision": "b38ff61aa327fbdc24f7b1dad7d04d1a",
+    "url": "/shrijayportfolio/static/media/heroku.b38ff61a.svg"
+  },
+  {
+    "revision": "676e8188f003ed5d4882a0d0f378dad3",
+    "url": "/shrijayportfolio/static/media/html-5.676e8188.svg"
+  },
+  {
+    "revision": "2d2e31cc63a96c7e6044f52d30fd21b9",
+    "url": "/shrijayportfolio/static/media/javascript.2d2e31cc.svg"
+  },
+  {
+    "revision": "a91263454cc87d5fb48be63c12def98b",
+    "url": "/shrijayportfolio/static/media/logow.a9126345.webp"
+  },
+  {
+    "revision": "82291019f34ab54a677c456a84127cc3",
+    "url": "/shrijayportfolio/static/media/material-ui-1.82291019.svg"
+  },
+  {
+    "revision": "3ea10fb644c05e7c947611dae8d58a45",
+    "url": "/shrijayportfolio/static/media/mern_tip_calc.3ea10fb6.webp"
+  },
+  {
+    "revision": "2985235df31b8742d893d5b7e80145c5",
+    "url": "/shrijayportfolio/static/media/mongodb.2985235d.svg"
+  },
+  {
+    "revision": "54d7bddcd3019ec3c3b23db00160ccef",
+    "url": "/shrijayportfolio/static/media/mssql.54d7bddc.svg"
+  },
+  {
+    "revision": "a123152868245dee71c441124a98ac3e",
+    "url": "/shrijayportfolio/static/media/nodejs.a1231528.svg"
+  },
+  {
+    "revision": "3429e8f62c98305834666720ad6d6235",
+    "url": "/shrijayportfolio/static/media/portfoliousingreact.3429e8f6.webp"
+  },
+  {
+    "revision": "b6039d04b0be03ee5dd8276f676a00f7",
+    "url": "/shrijayportfolio/static/media/profile.b6039d04.webp"
+  },
+  {
+    "revision": "146716bdf1a351db5705ec1e6a25ed08",
+    "url": "/shrijayportfolio/static/media/python.146716bd.svg"
+  },
+  {
+    "revision": "83fe61b05935f090b671b08aeb77b63d",
+    "url": "/shrijayportfolio/static/media/react-bootstrap.83fe61b0.svg"
+  },
+  {
+    "revision": "024196716802e272adb3cbefb81fd557",
+    "url": "/shrijayportfolio/static/media/react-router.02419671.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/shrijayportfolio/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "aa0a337603611296524a666b62a9ff23",
+    "url": "/shrijayportfolio/static/media/redux.aa0a3376.svg"
+  },
+  {
+    "revision": "b446ca5a397c1ef6fb94cd02c4e94335",
+    "url": "/shrijayportfolio/static/media/sass-1.b446ca5a.svg"
+  },
+  {
+    "revision": "8bce7aef886c3872c218e0e6ef8bdccb",
+    "url": "/shrijayportfolio/static/media/slide1.8bce7aef.webp"
+  },
+  {
+    "revision": "7256f309520276be70acd97720610bc6",
+    "url": "/shrijayportfolio/static/media/slide2.7256f309.webp"
+  },
+  {
+    "revision": "8e5b87b60151049a8df9ec0bd30978f0",
+    "url": "/shrijayportfolio/static/media/slide3.8e5b87b6.webp"
+  },
+  {
+    "revision": "3de182d2120823fe49c27eeb1765c53a",
+    "url": "/shrijayportfolio/static/media/typescript.3de182d2.svg"
+  }
+]);
